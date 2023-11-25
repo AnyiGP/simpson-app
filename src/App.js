1119f7ac16b72c63bf0bf4d31f06b53f23bd87c1
AnyiGP <angelesgp31@hotmail.com>
2023-11-25 07:16:44 -0300

    Add counter

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { CardCharacter } from "./components/CardCharacter";
 
 function App() {
   const [character, setcharacter] = useState([]);
+  const [count, setCount] = useState(0) 
   
   useEffect(() => {
     //first, consulta a la api
     const fetchData = async () => {
-      const res = await fetch("https://apisimpsons.fly.dev/api/personajes");
+      const res = await fetch(`https://apisimpsons.fly.dev/api/personajes?limit=3&page=${count}`);
       const data = await res.json();
       //console.log(data)
       setcharacter(data.docs);
@@ -19,13 +20,26 @@ function App() {
     // }
 
     fetchData();
-  }, []); //third
+  }, [count]); //third
 
-  console.log(character);
+  //console.log(character);
+
+  const handlerSuma = ()=>{
+    setCount(count+1)
+  }
+  const handlerResta = ()=>{
+    count==0?setCount(count):setCount(count-1)
+  }
+
 
   return (
     <>
       <h1>Los Simpson</h1>
+      <h2>{count}</h2>
+
+      <button onClick={handlerSuma}> + </button>
+      <button onClick={handlerResta} > - </button>
+
       <div className="cards">
       {character.map((character, id) => {
         return <CardCharacter key={id} character={character} />;
